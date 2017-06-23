68a6f4be7acb47ab7529f15a38eda099a9aa5bea
Matt <mdthomas@hotmail.ca>
2017-06-22 21:32:49 -0700

    Formatted data o/p including SSID.

--- wispy.py
+++ wispy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# WUDS - Wifi User Detection System, refactored.
+# Wispy v0.0.1
 
 import sys
 import subprocess
@@ -15,11 +15,12 @@ INTERFACE = ''
 
 def main():
     if len(sys.argv) != 2:
-        print('\nUsage: wuds.py <wlan-interface>\n')
+        print('\nUsage: wispy.py <wlan-interface>\n')
         sys.exit(1)
     else:
         INTERFACE = sys.argv[1]
         if enable_monitor_mode(INTERFACE):
+            print('OK')
             start_packet_capture()
         else:
             sys.exit(1)
@@ -27,14 +28,14 @@ def main():
 
 def start_packet_capture():
     print('Packet capture starting on ' + INTERFACE)
-    capture = pcapy.open_live(INTERFACE, 1514, 1, 0)
+    capture = pcapy.open_live(INTERFACE, 1514, 1, 1)
     capture.setfilter('subtype probe-req')
     while True:
         try:
+            header_type = capture.datalink()
             (header, pkt) = capture.next()
-            if capture.datalink() == 0x7F: # 127
-                packet_handler(header, pkt)
-                #packet_handler2(pkt)
+            if header_type == 0x7F and len(pkt) > 0: # 127
+                packet_handler(header, pkt)    
         except KeyboardInterrupt:
             disable_monitor_mode()
             break
@@ -44,13 +45,10 @@ def enable_monitor_mode(wifi_interface):
     #try:
         global INTERFACE
         INTERFACE = wifi_interface
-        print('Starting ' + INTERFACE + ' in monitor mode.')
-        #iw = subprocess.Popen(['ping', '127.0.0.1'], stdout=subprocess.PIPE)
-        iw = subprocess.Popen(['iw', 'dev', INTERFACE, 'interface', 'add', INTERFACE + 'mon', 'type', 'monitor'], stdout=subprocess.PIPE)
-        print(iw.communicate())
+        print('Starting ' + INTERFACE + ' in monitor mode...', end= ' ')
+        iw = subprocess.Popen(['iw', 'dev', INTERFACE, 'interface', 'add', INTERFACE + 'mon', 'type', 'monitor'])
         INTERFACE = wifi_interface + 'mon'
-        ifconfig = subprocess.Popen(['ifconfig', INTERFACE, 'up'], stdout=subprocess.PIPE)
-        print(ifconfig.communicate())
+        ifconfig = subprocess.Popen(['ifconfig', INTERFACE, 'up'])
         return True
     #except:
        # print('error stuff')
@@ -70,44 +68,21 @@ def disable_monitor_mode():
         
  
 def packet_handler(header, pkt):
-    # print(str(datetime.now()) + 'Packet analysis \n')
     ts = header.getts()
     ts = time.strftime('%Y-%m-%d %H:%M:%S:', time.localtime(ts[0])) + str(ts[1])
-    print(ts + ', Cap Size: %d, Pkt Size: %d' % (header.getcaplen(), header.getlen()))
-    #print(dir(radiotap))
-    print(pkt)
-    print('Byte 51')
-    print(pkt[51])
-    (pktlen, parsedPkt) = radiotap.radiotap_parse(pkt)
-    mac = radiotap.ieee80211_parse(pkt, pktlen)
-    print(parsedPkt)
-    print(mac)
+    (pktlen, parsed_pkt) = radiotap.radiotap_parse(pkt)
+    rtap_hdr = radiotap.ieee80211_parse(pkt, pktlen)
+    #print(ts + ', Cap Size: %d, Pkt Size: %d' % (header.getcaplen(), header.getlen()))
+    print(ts + ' MAC: %s CHAN: %d RSSI: %d SSID: ' % (rtap_hdr[1]['addr2'], parsed_pkt['chan_freq'], parsed_pkt['dbm_antsignal']), end=' ')
+    ssidlen = pkt[51]
+    if ssidlen > 0:
+        ssid = pkt[52:52+ssidlen].decode('utf-8')
+        print(ssid)
+    else:
+        print('<None>')   
 
-
-
-def packet_handler2(pkt):
-    print('pkt handler 2')
-    # print(str(datetime.now()) + 'Packet analysis \n')
-    rtlen = struct.unpack('h', pkt[2:4])[0]
-    ftype = (pkt[rtlen] >> 2) & 3
-    stype = pkt[rtlen] >> 4
-    # check if probe request
-    if ftype == 0 and stype == 4:
-        rtap = pkt[:rtlen]
-        frame = pkt[rtlen:]
-        # parse bssid
-        #bssid = frame[10:16].encode('hex')
-        bssid = frame[10:16]
-        bssid = ':'.join([bssid[x:x + 2] for x in range(0, len(bssid), 2)])
-        # parse rssi
-        rssi = struct.unpack('b', rtap[-6:-5])[0]  # there's another value at -3:-2 to investigate
-        # parse essid
-        essid = frame[26:26 + ord(frame[25])] if ord(frame[25]) > 0 else '<None>'
-        # build data tuple
-        data = (bssid, rssi, essid)
-        print(data)
-
-        
+def channel_hop(channel):
+    iwconfig = subprocess.Popen(['iwconfig', INTERFACE, 'channel', channel])
         
 if __name__ == '__main__':
     main()
