a9f1ac5e934696bcc23f6788fd20008097bd28cd
mdtomo <mdthomas@hotmail.ca>
2017-06-25 11:37:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='wispy',
     version='0.0.1',
-    packages=['wispy', 'radiotap', 'vht', 'pcapy'],
     license='MIT',
     long_description=open('README.md').read(),
+    packages=['wispy', 'python_radiotap', 'pcapy'],
 )
