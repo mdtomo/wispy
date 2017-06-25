dcab952776afe91b04f4614ec887357e83927678
mdtomo <mdthomas@hotmail.ca>
2017-06-25 11:41:10 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     version='0.0.1',
     license='MIT',
     long_description=open('README.md').read(),
-    packages=['wispy', 'python_radiotap', 'pcapy'],
+    packages=['wispy', 'wispy.python_radiotap', 'pcapy'],
 )
